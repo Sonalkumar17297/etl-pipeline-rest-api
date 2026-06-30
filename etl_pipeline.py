6b527b76f2e896597b6f494298abca71ec59ba75
Sonalkumar17297 <91379894+Sonalkumar17297@users.noreply.github.com>
2026-06-30 18:36:19 +0530

    etl_pipeline.py

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[40]:
-
 
 import requests
 
@@ -10,26 +8,17 @@ response = requests.get('https://jsonplaceholder.typicode.com/users')
 users_data = response.json()
 
 
-# In[41]:
 
 
 print(users_data[0])
 
-
-# In[42]:
 
 
 response = requests.get('https://jsonplaceholder.typicode.com/posts')
 posts_data = response.json()
 
 
-# In[43]:
-
-
 print(posts_data[0])
-
-
-# In[44]:
 
 
 import pandas as pd
@@ -41,8 +30,6 @@ print(users_df.head())
 print(posts_df.head())
 
 
-# In[45]:
-
 
 users_df['city'] = users_df['address'].apply(lambda x: x['city'])
 users_df['company_name'] = users_df['company'].apply(lambda x: x['name'])
@@ -50,20 +37,12 @@ users_df['zipcode'] = users_df['address'].apply(lambda x: x['zipcode'])
 users_df[['id', 'name', 'city', 'company_name','zipcode']]
 
 
-# In[46]:
-
 
 users_clean = users_df[['id', 'name', 'username', 'email', 'city', 'company_name', 'zipcode']]
 
 
-# In[47]:
-
 
 users_clean.head()
-
-
-# In[48]:
-
 
 result = posts_df.merge(
     users_clean,
@@ -75,25 +54,14 @@ result = posts_df.merge(
 result = result.rename(columns={'id_x': 'post_id', 'id_y': 'user_id'})
 
 
-# In[49]:
-
-
 print(result.columns)
 
-
-# In[50]:
 
 
 final_df = result[['user_id', 'name', 'city', 'company_name', 'post_id', 'title']]
 
 
-# In[51]:
-
-
 final_df.head()
-
-
-# In[52]:
 
 
 import sqlite3
@@ -102,16 +70,11 @@ conn = sqlite3.connect('project1.db')   # creates a database FILE
 final_df.to_sql('user_posts', conn, if_exists='replace', index=False)
 
 
-# In[53]:
-
-
 check = pd.read_sql('SELECT * FROM user_posts LIMIT 5', conn)
 print(check)
 
 
 # Which city's users post the most?
-
-# In[54]:
 
 
 query = """
@@ -127,8 +90,6 @@ print(result)
 
 # Find each company_name and how many TOTAL posts their employees have written, sorted highest to lowest.
 
-# In[56]:
-
 
 query2 = """
 SELECT company_name, COUNT(*) AS post_count
@@ -143,9 +104,6 @@ print(result2)
 
 # Find the user (name) who has written the MOST posts overall — just the top 1 user.
 
-# In[58]:
-
-
 query3 = """
 SELECT name, COUNT(*) AS post_count
 FROM user_posts
@@ -155,10 +113,3 @@ ORDER BY post_count DESC limit 1;
 
 result3 = pd.read_sql(query3, conn)
 print(result3)
-
-
-# In[ ]:
-
-
-
-
